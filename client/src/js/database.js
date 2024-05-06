48ed33b34f3b9006427b8e3c443e35dfa34345e5
Gibin <gibinmgeorge2001@gmail.com>
2024-05-06 14:01:27 -0500

    working on js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,7 @@
 import { openDB } from 'idb';
 
+// IndexedDB initialization
+
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -13,9 +15,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+
+export const putDb = async (content) => {
+  const contactDb = await openDB('jate', 1);
+  const txt = contactDb.transaction('jate', 'readwrite');
+  const store = txt.objectStore('jate');
+  const request = store.add({ content: content });
+  const result = await request;
+  console.log('Data saved. Save IndexedDB ID:', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+export const getDb = async () => {
+  const contactDb = await openDB('jate', 1);
+  const txt = contactDb.transaction('jate', 'readonly');
+  const store = txt.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('Loading last save from IndexedDB:', result[result.length - 1]);
+  return result;
+}
 
 initdb();
